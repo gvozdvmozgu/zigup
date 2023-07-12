054c81bf618ae18695e88ae162296bd4a1592921
gvozdvmozgu <gvozdvmozgu@gmail.com>
2023-07-12 16:39:45 -0400

    simplify

--- src/main.rs
+++ src/main.rs
@@ -3,17 +3,17 @@ use std::fs::File;
 
 use anyhow::Context as _;
 
-const HOST_TRIPLE: &'static str = env!("HOST_TRIPLE");
-const DOWNLOAD_INDEX_URL: &'static str = "https://ziglang.org/download/index.json";
+const HOST_TRIPLE: &str = env!("HOST_TRIPLE");
+const DOWNLOAD_INDEX_URL: &str = "https://ziglang.org/download/index.json";
 
 type DownloadIndex = indexmap::IndexMap<String, HashMap<String, serde_json::Value>>;
 
 fn main() -> anyhow::Result<()> {
     let download_index: DownloadIndex = ureq::get(DOWNLOAD_INDEX_URL).call()?.into_json()?;
 
-    let (version, release) = download_index.into_iter().nth(1).unwrap();
-    let file =
-        release.get(HOST_TRIPLE).with_context(|| format!("Unsupported {HOST_TRIPLE} platform."))?;
+    let (version, releases) = download_index.into_iter().nth(1).unwrap();
+    let release =
+        releases.get(HOST_TRIPLE).with_context(|| format!("unsupported {HOST_TRIPLE} platform"))?;
 
     println!(
         "Current installation options:
@@ -21,7 +21,7 @@ fn main() -> anyhow::Result<()> {
     default toolchain: {version}"
     );
 
-    let tarball: &str = file["tarball"].as_str().unwrap();
+    let tarball = release["tarball"].as_str().unwrap();
     let filename = tarball.split('/').last().unwrap();
 
     let mut reader = ureq::get(tarball).call()?.into_reader();
