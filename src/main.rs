6aa64cc78a8661df46d8493722f4c43e7e45db0b
gvozdvmozgu <gvozdvmozgu@gmail.com>
2025-04-07 11:50:13 -0700

    Add support for downloading and extracting ZIP archives

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@
 
 mod path;
 
+use std::io::Seek as _;
+
 use anyhow::Context as _;
 
 const HOST_TRIPLE: &str = env!("HOST_TRIPLE");
@@ -11,6 +13,9 @@ type DownloadIndex =
     indexmap::IndexMap<String, std::collections::HashMap<String, serde_json::Value>>;
 
 fn main() -> anyhow::Result<()> {
+    std::fs::create_dir_all(path::toolchains())
+        .with_context(|| "failed to create toolchains directory")?;
+
     let download_index: DownloadIndex = ureq::get(DOWNLOAD_INDEX).call()?.into_json()?;
 
     let (version, releases) = download_index.into_iter().nth(1).unwrap();
@@ -24,17 +29,50 @@ fn main() -> anyhow::Result<()> {
     );
 
     let tarball = release["tarball"].as_str().unwrap();
-    let reader = ureq::get(tarball).call()?.into_reader();
+    let response = ureq::get(tarball).call()?;
 
     if tarball.ends_with(".tar.xz") {
         use tar::Archive;
         use xz2::read::XzDecoder;
 
-        let mut archive = Archive::new(XzDecoder::new(reader));
+        let mut archive = Archive::new(XzDecoder::new(response.into_reader()));
         archive.unpack(path::toolchains())?;
+    } else if tarball.ends_with(".zip") {
+        let mut archive = std::fs::File::options()
+            .truncate(true)
+            .write(true)
+            .read(true)
+            .open(path::toolchains().join("zig.zip"))?;
+
+        download_file(response, &mut archive)?;
+        archive.seek(std::io::SeekFrom::Start(0))?;
+
+        let mut zip = zip::ZipArchive::new(archive)?;
+        zip.extract(path::toolchains())?;
     } else {
         anyhow::bail!("unsupported archive format")
     };
+
+    Ok(())
+}
+
+pub fn download_file(
+    response: ureq::Response,
+    writer: &mut impl std::io::Write,
+) -> std::io::Result<()> {
+    const DOWNLOAD_TEMPLATE: &str = "{msg} {spinner:.green} [{elapsed_precise}] \
+                                     [{wide_bar:.cyan/blue}] {bytes}/{total_bytes} ({eta})";
+
+    let length: u64 = response.header("Content-Length").unwrap().parse().unwrap();
+
+    let bar = indicatif::ProgressBar::new(!0);
+    bar.set_message("Downloading");
+    bar.set_style(
+        indicatif::ProgressStyle::with_template(DOWNLOAD_TEMPLATE).unwrap().progress_chars("##-"),
+    );
+    bar.set_length(length);
+
+    std::io::copy(&mut bar.wrap_read(response.into_reader()), writer).unwrap();
 
     Ok(())
 }
